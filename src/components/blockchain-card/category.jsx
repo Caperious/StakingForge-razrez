f1801899fb90f1e708429026b035380948117374
David Rudman <dvd.rudman@gmail.com>
2024-07-03 15:37:29 +0200

    Fixed text color on category

--- src/components/blockchain-card/category.jsx
+++ src/components/blockchain-card/category.jsx
@@ -1,3 +1,3 @@
 export const Category = ({category}) => {
-  return <span className={'text-xs text-center font-light bg-primary-secondary-dark p-0.5 mt-0.5 px-2 rounded-full break-keep text-nowrap'}>{category}</span>
+  return <span className={'text-xs text-center text-white font-light bg-primary-secondary-dark p-0.5 mt-0.5 px-2 rounded-full break-keep text-nowrap'}>{category}</span>
 }
